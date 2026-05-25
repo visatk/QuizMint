709f880315188684c331aea6fb8877ff0138a3ed
visatk <visatk29@gmail.com>
2026-05-25 10:28:24 +0600

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,12 +3,18 @@ export {};
 declare global {
   interface Window {
     ezstandalone: {
-      hasInit: boolean;
-      define: (id: number) => void;
-      display: () => void;
-      enable: () => void;
-      destroyAll: () => void; // Added
-      refresh: () => void;    // Added
+      cmd: Array<() => void>;
+      hasInit?: boolean;
+      define?: (id: number) => void;
+      display?: () => void;
+      enable?: () => void;
+      destroyAll?: () => void;
+      refresh?: () => void;
+    };
+    ezoic?: {
+      rewarded?: {
+        requestAndShow: (cb: (rewardGranted: boolean) => void) => void;
+      };
     };
   }
 }
