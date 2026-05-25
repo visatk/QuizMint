5a6c35b9741ea019ef7a31ea93cec04b8dcaac6a
visatk <visatk29@gmail.com>
2026-05-25 06:14:13 +0600

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,9 +3,6 @@ import { getCloudflareContext } from "@opennextjs/cloudflare";
 import * as schema from "./schema";
 
 export function getDB() {
-  // OpenNext environment bindings থেকে D1 ডাটাবেস এক্সট্রাক্ট করা
   const { env } = getCloudflareContext();
-  
-  // Drizzle ORM এর সাথে D1 কানেক্ট করা এবং Schema ম্যাপ করা
   return drizzle(env.DB, { schema }); 
 }
